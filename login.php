ff16fe3f45568f83d8c816b81236149fb1751671
EgiEm <egiem@users.noreply.github.com>
2026-07-01 11:00:02 +0200

    User authentication and login verification script

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+require_once ("config.php");
+session_start();
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $_POST['username'];
+    $password = md5($_POST['password']); //Hash password
+
+    $sql = "SELECT * FROM admin WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+    if($result->num_rows == 1){
+        $_SESSION['admin_logged_in'] = true;
+        header("Location: admin.php");
+        exit();
+    }else{
+        $error = "Invalid Username or Password!";
+    }
+}
+?>
